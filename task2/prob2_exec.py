366ad657642f0f0d65ac80ac88faf26f6a1f464d
Irvit1 <irvit2002gupta@gmail.com>
2024-10-04 22:01:38 +0530

    implemented the song class

--- task2/prob2_exec.py
+++ task2/prob2_exec.py
@@ -1,6 +1,9 @@
 """
-Create a singleton for Song Management System as in single instance
+Create a singleton for Song Management System as in single
 
 Create a menu driven function which will be used to run the function
 
+
 """
+
+
