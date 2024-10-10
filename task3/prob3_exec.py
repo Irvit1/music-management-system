ade2677122349ce9d728bb8bed67791afac247d4
Irvit1 <irvit2002gupta@gmail.com>
2024-10-10 09:35:15 +0530

    template to implement task 3

--- task3/prob3_exec.py
+++ task3/prob3_exec.py
@@ -4,3 +4,26 @@ Create a singleton for Song Management System User as in single instance
 Create a menu driven function which will be used to run the function
 
 """
+
+from task3.song_management_sys_user import SongManagementSystemUser
+
+
+def menu():
+    print("\n--- User Menu ---")
+    print("1. Search for a Song by Title")
+    print("2. Search for All Songs by an Artist")
+    print("3. Exit")
+
+def main():
+    filename = "songs.txt"
+    user = SongManagementSystemUser(filename)
+
+    # Now just implement a while loop to run the menu and call the respective functions
+    # choice 1 -> ask for title and call search_song_by_title
+    # choice 2 -> ask for artist and call search_songs_by_artist
+    # choice 3 -> exit the program
+    # Also, check if the choice is invalid and print a message to the user
+
+
+if __name__ == "__main__":
+    main()
--- task3/song_management_sys_user.py
+++ task3/song_management_sys_user.py
@@ -10,3 +10,29 @@ Create a class named SongsManagementSystemUser
     function search_songs_by_artist
 
 """
+
+from task2.song_management_sys import SongsManagementSystem
+
+class SongManagementSystemUser(SongsManagementSystem):
+    # fetch song data in init method
+    def __init__(self, filename: str) -> None:
+        super().__init__()
+        # Implement a try except in init method and raise FileNotFoundErrorCustom exception if file is not found or InvalidFileFormatError if file format is incorrect
+        self.load_song_data(filename)
+
+    # search song by title
+    def search_song_by_title(self, title: str) -> None:
+        # Implement a try except in this function and raise SongNotFoundException if song is not found
+        # In try, if the result is available, print the result
+        print('temp')
+    
+
+    def search_songs_by_artist(self, artist: str) -> None:
+        # Implement a try except in this function and raise ArtistNotFoundException if artist is not found
+        # In try, if the result is available, return the result
+        print('temp')
+
+
+
+
+    
